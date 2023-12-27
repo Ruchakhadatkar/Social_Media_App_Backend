165b6e67f4678155239b086556473c6ca72b75d6
Rucha Khadatkar <ruchakhadatkar83@gmail.com>
2023-12-27 15:27:13 +0530

    Friend request controller compeleted

--- controller/friendRequestController.js
+++ controller/friendRequestController.js
@@ -1,7 +1,8 @@
 const Request = require("../models/friendRequestModel");
+const Friends = require("../models/friendsModel");
 
 const sendFriendRequest = async (req, res) => {
-  const { senderUserId, receiverUserId, status, requestDate } = req.body;
+  const { senderUserId, receiverUserId, status} = req.body;
 
   try {
     if (!senderUserId && !receiverUserId) {
@@ -19,11 +20,54 @@ const sendFriendRequest = async (req, res) => {
   }
 };
 
-const acceptFriendRequest = (req,res)=>{
-   
-}
+const acceptFriendRequest = async (req, res) => {
+  const { reqId, status } = req.body;
+
+  try {
+    const data = await Request.findByIdAndUpdate(reqId, { status });
+    if (data) {
+      const friend = new Friends({
+        userId1: data.senderUserId,
+        userId2: data.receiverUserId,
+      });
+      await friend.save();
+      return res.json({ data: friend, success: true });
+    }
+  } catch (error) {
+    return res.json({ error: error.message });
+  }
+};
+
+const declineFriendRequest = async (req, res) => {
+  const { reqId } = req.body;
+
+  try {
+    if (!reqId) {
+      return res.json({ error: "Request Id is required", success: false });
+    }
+    const data = await Request.findByIdAndDelete(reqId);
+    return res.json({ data, success: true });
+  } catch (error) {
+    return res.json({ error: error.message });
+  }
+};
+
+const getAllFriendRequest = async (req, res) => {
+  const userId = req.query.id;
+  try {
+    if (!userId) {
+      return res.json({ error: "User Id is required", success: false });
+    }
+    const data = await Request.find({ receiverUserId: userId });
+    return res.json({ data, success: true });
+  } catch (error) {
+    return res.json({ error: error.message });
+  }
+};
 
 module.exports = {
   sendFriendRequest,
   acceptFriendRequest,
+  declineFriendRequest,
+  getAllFriendRequest,
 };
--- routes/friendRequestRoute.js
+++ routes/friendRequestRoute.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const requireAuth = require('../middleware/requireAuth')
-const { sendFriendRequest, acceptFriendRequest } = require('../controller/friendRequestController')
+const { sendFriendRequest, acceptFriendRequest, declineFriendRequest, getAllFriendRequest } = require('../controller/friendRequestController')
 
 const router = express.Router()
 
 router.post("/", sendFriendRequest )
 router.put("/", acceptFriendRequest)
+router.delete("/", declineFriendRequest)
+router.get("/", getAllFriendRequest)
 
 module.exports = router
